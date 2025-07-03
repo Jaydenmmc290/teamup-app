a59633cfbe88847921fb14e10b56375ddc73dad0
Jayden McBride <jaydenmcbride773@gmail.com>
2025-07-03 18:49:06 -0400

    Add root URL route to homepage

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("Welcome to the TeamUp API!")),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
